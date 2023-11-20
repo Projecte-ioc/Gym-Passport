a621191fe8b595139a3e2f7952364b7ee28ac45e
MeritxellGonzalez <meritxellgonzalezrobert@gmail.com>
2023-11-20 13:54:38 +0100

    añado endpoint para filtrar por usuario creador

--- projecte_mic/api_events_tea3.py
+++ projecte_mic/api_events_tea3.py
@@ -10,7 +10,7 @@ db = Connexion()
 #  __keys_events__ = ['id', 'name', 'whereisit', 'schedule', 'qty_max_attendes', 'qty_got_it', 'user_id',
 #                      'gym_id', 'done','date', 'hour']
 
-@app.route('/obtener_eventos')
+@app.route('/obtener_eventos', methods=['POST'])
 def get_all_events():
     token = request.headers.get('Authorization')
     rol_user, id, user_name, gym_name = db.validate_rol_user(token)
@@ -21,6 +21,16 @@ def get_all_events():
     else:
         return jsonify({'message': 'No es possible recuperar les dades'}), 404
 
+@app.route('/filtrar_eventos', methods=['POST'])
+def get_filtered_events():
+    token = request.headers.get('Authorization')
+    rol_user, id, user_name, gym_name = db.validate_rol_user(token)
+    user_name_params = request.args.get('user_name')
+    id_gym_user_params = db.get_elements_filtered(user_name_params, User.__table_name__, 'user_name', 'gym_id')
+    if rol_user == 'admin' and id_gym_user_params == id:
+        id_user = db.get_elements_filtered(user_name_params, User.__table_name__, 'user_name', 'id')
+        results = db.get_elements_filtered(id_user, GymEvent.__table_name__,'user_id', '*')
+        print(results)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
