49b2318ba72260183570deec3b3d85056b1f5081
MeritxellGonzalez <meritxellgonzalezrobert@gmail.com>
2023-11-20 13:42:47 +0100

    BUG FIXED en api_events_tea3.py

--- projecte_mic/api_events_tea3.py
+++ projecte_mic/api_events_tea3.py
@@ -9,17 +9,15 @@ db = Connexion()
 
 #  __keys_events__ = ['id', 'name', 'whereisit', 'schedule', 'qty_max_attendes', 'qty_got_it', 'user_id',
 #                      'gym_id', 'done','date', 'hour']
-# TODO revisar error 'unhasable type: list'
+
 @app.route('/obtener_eventos')
 def get_all_events():
     token = request.headers.get('Authorization')
     rol_user, id, user_name, gym_name = db.validate_rol_user(token)
     results = db.get_elements_filtered(id, GymEvent.__table_name__, 'gym_id', '*')
     if results:
-        print(type(results))
-        # results_dict = [dict(zip(GymEvent.__keys_events__, row)) for row in results]
-        print(str(results))
-        return jsonify({results}), 200
+        results_dict = [dict(zip(GymEvent.__keys_events__, row)) for row in results]
+        return jsonify(results_dict), 200
     else:
         return jsonify({'message': 'No es possible recuperar les dades'}), 404
 
